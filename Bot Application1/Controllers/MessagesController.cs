ceafd461b2952e17cf64e6bdf6ed2d12191d2219
YasirZardari <yasirzardari@gmail.com>
2017-02-24 14:47:16 +0000

    changed files

--- Bot Application1/Controllers/MessagesController.cs
+++ Bot Application1/Controllers/MessagesController.cs
@@ -26,7 +26,7 @@ namespace Bot_Application1
                 int length = (activity.Text ?? string.Empty).Length;
 
                 // return our reply to the user
-                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters");
+                Activity reply = activity.CreateReply($"You sent {activity.Text} which was {length} characters!");
                 await connector.Conversations.ReplyToActivityAsync(reply);
             }
             else
